0429b9c2868314ad10cefa7a0beb401759184fdb
LaupWing <loc_strict@hotmail.com>
2023-02-02 10:14:35 +0100

    updated

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { LinearGradient } from "expo-linear-gradient"
-import { SafeAreaView, Text } from "react-native"
+import { SafeAreaView, StatusBar, Text } from "react-native"
 
 const App = () => {
    return (
-      <SafeAreaView className="bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center flex-1">
+      <SafeAreaView className="flex items-center justify-center flex-1 w-full">
+         {/* <StatusBar/> */}
          <LinearGradient
+            className="flex-1 w-full"
             colors={["#6366f1", "#a855f7", "#ec4899"]}
          >
-            {/* <StatusBar/> */}
             <Text>Open up App.tsx to start working on your app!</Text>
          </LinearGradient>
       </SafeAreaView>
