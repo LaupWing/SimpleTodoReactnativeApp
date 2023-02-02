7d0aa6c09ff48c68c8cc2b57d0e4a8687af59e14
LaupWing <loc_strict@hotmail.com>
2023-02-02 10:24:15 +0100

    udpate

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
 import { LinearGradient } from "expo-linear-gradient"
-import { SafeAreaView, StatusBar, Text } from "react-native"
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native"
+import { AntDesign } from "@expo/vector-icons"
 
 const App = () => {
    return (
       <SafeAreaView className="flex items-center justify-center flex-1 w-full">
-         {/* <StatusBar/> */}
          <LinearGradient
-            className="flex-1 w-full"
+            className="flex-1 w-full justify-center"
             colors={["#6366f1", "#a855f7", "#ec4899"]}
          >
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <View className="p-4 bg-white rounded mx-6 shadow">
+               <Text className="font-bold text-neutral-700 text-center text-2xl">Login</Text>
+
+               <View className="p-1 mt-4">
+                  <TouchableOpacity className="bg-neutral-50 py-2.5 flex border flex-row border-neutral-200 px-2 rounded">
+                     <AntDesign
+                        name="google"
+                        size={20}
+                        color="red"
+                     />
+                     <Text className="ml-1">Login with Google</Text>
+                  </TouchableOpacity>
+               </View>
+            </View>
          </LinearGradient>
       </SafeAreaView>
    )
